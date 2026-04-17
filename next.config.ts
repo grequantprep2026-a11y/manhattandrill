ad97fed92dd5c5e84f26e3f4068575db42ab8a01
Lunora00 <feyntrain@gmail.com>
2026-04-17 15:47:21 +0530

    Remove deprecated eslint and typescript config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ const nextConfig = {
   allowedDevOrigins: [
     "untraceried-braxton-semirarely.ngrok-free.dev",
   ],
-    typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 module.exports = nextConfig;
